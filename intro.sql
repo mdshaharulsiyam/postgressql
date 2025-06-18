04ff8df76a070bc5156570eb0d5c711ecd064ea6
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-18 09:48:25 -0600

    intro

--- intro.sql
+++ intro.sql
@@ -1,4 +1,10 @@
 -- Active: 1750260753519@@127.0.0.1@5432@postgres
 
--- create table
-CREATE DATABASE practice
+-- create database
+-- CREATE DATABASE practice;
+
+-- switch database
+-- USE practice;
+
+-- rename database
+ALTER DATABASE practice RENAME TO practice_renamed;
