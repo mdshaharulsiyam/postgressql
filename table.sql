befe51d3e41e322a14a2175b1ef7082a2d9b95d7
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-25 10:05:04 +0600

    curse department and student table created

--- table.sql
+++ table.sql
@@ -11,3 +11,71 @@ CREATE TABLE "Employee" (
   department_id INT,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES "Department"(department_id) ON DELETE CASCADE
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE "Courses"(
+  course_id SERIAL PRIMARY KEY,
+  course_name VARCHAR(100) NOT NULL,
+  credits INT CHECK(credits BETWEEN 1 AND 10)
+)
+
+CREATE TABLE "Departments"(
+  department_id SERIAL PRIMARY KEY,
+  department_name VARCHAR(50)
+)
+
+
+CREATE TABLE "Students" (
+  student_id SERIAL PRIMARY KEY,
+  student_name VARCHAR(30) NOT NULL,
+  roll INT NOT NULL,
+  fees INT NOT NULL,
+  about TEXT,
+  date_of_birth DATE CHECK (
+        date_of_birth BETWEEN '1900-01-01' AND CURRENT_DATE
+  ),
+  email VARCHAR(60),
+  department_id INT,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES "Department"(department_id) ON DELETE CASCADE
+)
+
