63d107866e44ce19b95f3735369e8fc4366eaee5
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-06-29 09:27:51 -0600

    count all student

--- aggregation.sql
+++ aggregation.sql
@@ -1,1 +1,13 @@
 SELECT COUNT(*) AS total, MAX(fees) AS max_fee, MIN(fees) AS min_fee, AVG(fees) AS avg_fee FROM "Students";
+
+SELECT 
+  COUNT(*) AS student_per_dept,
+    d.department_name AS department
+FROM 
+  "Students" s
+LEFT JOIN 
+  "Departments" d ON d.department_id = s.department_id
+GROUP BY 
+  d.department_name;
+
+
