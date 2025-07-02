f82b9eeb9fa5b93fef7cbc80870a59ebf4cc05b7
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-07-02 16:03:59 +0600

    problem 10 solve

--- aggregation.sql
+++ aggregation.sql
@@ -17,3 +17,12 @@ GROUP BY
 SELECT EXTRACT(YEAR FROM AGE(date_of_birth)) AS age_in_years ,student_name,date_of_birth FROM "Students" WHERE date_of_birth BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT COUNT(*) AS total_student , EXTRACT(YEAR FROM date_of_birth) AS years FROM "Students" GROUP BY years;
+
+SELECT
+ c.course_name ,
+ c.course_id ,
+  (SELECT AVG(e.grade) FROM "Enrollments" e WHERE e.course_id = c.course_id) 
+  AS average_grade
+   FROM "Courses" c 
+   ORDER BY average_grade DESC LIMIT 5;
+   
